576393b70e92aa1756594296713262f546f11d1a
nachomirsol <nachomirsol@gmail.com>
2018-03-07 11:26:11 +0100

    updates

--- staff/nacho/users-api-server-0-mongoose/src/index.js
+++ staff/nacho/users-api-server-0-mongoose/src/index.js
@@ -2,26 +2,20 @@ require('dotenv').config()
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const { MongoClient } = require('mongodb')
 const uuid = require('uuid/v4')
 const { success, fail } = require('./api-utils')
 
-const host = process.env.MONGO_HOST
-const port = process.env.MONGO_PORT
-const database = process.env.MONGO_DB
-const collection = process.env.MONGO_COL
+const mongoose = require('mongoose')
 
-MongoClient.connect(`mongodb://${host}:${port}`, (err, conn) => {
-    if (err) throw err
-
-    const db = conn.db(database)
+mongoose.connect('mongodb://localhost/bootcamp')
+const User = mongoose.model('User', { id: String, name: String, surname: String, email: String, username: String, password: String })
 
     const app = express()
 
     app.get('/api/users', (req, res) => {
-        db.collection(collection).find().project({ _id: 0, id: 1, name: 1, surname: 1, email: 1, username: 1 }).toArray((err, users) => {
-            if (err) return res.json(fail(err))
 
+        User.find({}, (err, users) => {
+            if (err) return res.json(fail(err))
             res.json(success(users))
         })
     })
@@ -43,14 +37,14 @@ MongoClient.connect(`mongodb://${host}:${port}`, (err, conn) => {
             .then(() => {
                 validate({ name, surname, email, username, password })
 
-                return db.collection(collection).findOne({ username })
+                return User.findOne({ username })
             })
             .then(user => {
                 if (user) throw Error('username already exists')
 
                 const id = uuid()
 
-                return db.collection(collection).insert({ id, name, surname, email, username, password })
+                return User.create({ id, name, surname, email, username, password })
                     .then(() => id)
             })
             .then(id => {
@@ -69,17 +63,17 @@ MongoClient.connect(`mongodb://${host}:${port}`, (err, conn) => {
             .then(() => {
                 validate({ id, name, surname, email, username, password, newUsername, newPassword })
 
-                return db.collection(collection).findOne({ username: newUsername })
+                return User.findOne({ username: newUsername })
             })
             .then(user => {
                 if (user) throw Error('username already exists')
 
-                return db.collection(collection).findOne({ id })
+                return User.findOne({ id })
             })
             .then(user => {
                 if (user.username !== username || user.password !== password) throw Error('username and/or password wrong')
 
-                return db.collection(collection).updateOne({ id }, { $set: { name, surname, email, username: newUsername, password: newPassword } })
+                return User.findOneAndUpdate({ id }, { $set: { name, surname, email, username: newUsername, password: newPassword } })
             })
             .then(() => {
                 res.json(success())
@@ -97,7 +91,7 @@ MongoClient.connect(`mongodb://${host}:${port}`, (err, conn) => {
             .then(() => {
                 validate({ id, username, password })
 
-                return db.collection(collection).findOne({ username })
+                return User.findOne({ username })
             })
             .then(user => {
                 if (!user) throw Error('user does not exist')
@@ -106,7 +100,7 @@ MongoClient.connect(`mongodb://${host}:${port}`, (err, conn) => {
 
                 if (user.username !== username || user.password !== password) throw Error('username and/or password wrong')
 
-                return db.collection(collection).deleteOne({ id })
+                return User.deleteOne({ id })
             })
             .then(() => {
                 res.json(success())
@@ -123,7 +117,7 @@ MongoClient.connect(`mongodb://${host}:${port}`, (err, conn) => {
             .then(() => {
                 validate({ id })
 
-                return User.findById(id , {})
+                return User.findOne({ id },  { _id: 0, password: 0 } )
             })
             .then(user => {
                 if (!user) throw Error('user does not exist')
@@ -138,4 +132,3 @@ MongoClient.connect(`mongodb://${host}:${port}`, (err, conn) => {
     const port = process.env.PORT
 
     app.listen(port, () => console.log(`users api running on port ${port}`))
-})
